2682bc62148dd137e178baa52bed4087a1d34cf4
webdevs1-oleksandr-shaida <webdevs1.oleksandr.shaida@gmail.com>
2019-06-15 19:36:07 +0300

    Завдання 2

--- shaida-oleksandr/js/main.js
+++ shaida-oleksandr/js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
 
+    //Завдання 1
     var stack = $("body>section");
-    
-    // section #id_attr have DIVs: 2
-    for(let i = 0; i < stack.length; i++){
+
+    for(var i = 0; i < stack.length; i++){
         console.log("section "+ stack[i].getAttribute("id") + " have DIVs: " + stack[i].getElementsByTagName('div').length);
         
-    }
-   
+    };
+
+    //Завдання 2
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+        return false;
+    });
+    
 });
