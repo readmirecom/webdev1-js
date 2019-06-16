60c02345e502642070374619748756a7bdc6059c
vitalik-pc <webdevs1.vitalik.koshkin@gmail.com>
2019-06-16 21:15:34 +0300

    Finish JS

--- vitalik-koshkin/js/main.js
+++ vitalik-koshkin/js/main.js
@@ -3,7 +3,7 @@
 	(document).ready(function(){
     	var dump = $("body>section");
     	for(let i = 0; i < dump.length; i++){
-        	console.log("section #"+ dump[i].getAttribute("id") + " have DIVs: " + stack[i].getElementsByTagName('div').length);
+        	console.log("section #"+ dump[i].getAttribute("id") + " have DIVs: " + dump[i].getElementsByTagName('div').length);
     	}
 	});
 
