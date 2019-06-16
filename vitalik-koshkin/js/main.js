0694c16e0e1751c291d4e0d7ed53ccbc20bdf9d1
vitalik-pc <webdevs1.vitalik.koshkin@gmail.com>
2019-06-16 20:48:56 +0300

    Task 2

--- vitalik-koshkin/js/main.js
+++ vitalik-koshkin/js/main.js
@@ -1,6 +1,19 @@
+//1. Вивід кількості дочірніх елементів
+
 (document).ready(function(){
     var dump = $("body>section");
     for(let i = 0; i < dump.length; i++){
         console.log("section #"+ dump[i].getAttribute("id") + " have DIVs: " + stack[i].getElementsByTagName('div').length);
     }
 });
+
+//2. Smooth-scroll навігація
+
+	var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+        return false;
+    });
+    
