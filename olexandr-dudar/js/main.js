28295d9a28a6aa9ef2405f360e67ce809c12f3b6
just-n1ce <lxromanyuk@gmail.com>
2019-06-17 11:54:35 +0300

    Task 3

--- olexandr-dudar/js/main.js
+++ olexandr-dudar/js/main.js
@@ -1,17 +1,22 @@
-//Task 1
-$('section').each(function(){
-	let $section = $(this);
-	let n = $section.find('div').length;
-	console.log('section #'+$section.attr('id')+' have DIVs: '+n);
-});
+	//Task 1
+	$('section').each(function(){
+		let $section = $(this);
+		let n = $section.find('div').length;
+		console.log('section #'+$section.attr('id')+' have DIVs: '+n);
+	});
 
 
-//Task 2
+	//Task 2
 	var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 800);
-        return false;
-    });
+	$('a[href*="#"]').click(function() {
+		$page.animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 800);
+		return false;
+	});
 
+	//Task 3
+	$('.works-item ').on('click','a',function(event){
+		$(this).parent().parent().parent().children('img').toggleClass('invisible');
+		$(this).toggleClass('text-danger');
+	});
