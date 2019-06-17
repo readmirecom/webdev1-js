3a60ec4f942cf412046b7e097fdced3b031680bb
just-n1ce <lxromanyuk@gmail.com>
2019-06-17 11:42:00 +0300

    Task 1,2

--- olexandr-dudar/js/main.js
+++ olexandr-dudar/js/main.js
@@ -1,6 +1,17 @@
-document.ready(function(){
-    var dump = $("body>section");
-    for(var i = 0; i < dump.length; i++){
-        console.log("section #"+ dump[i].getAttribute("id") + " have DIVs: " + stack[i].getElementsByTagName('div').length);
-    }
+//Task 1
+$('section').each(function(){
+	let $section = $(this);
+	let n = $section.find('div').length;
+	console.log('section #'+$section.attr('id')+' have DIVs: '+n);
 });
+
+
+//Task 2
+	var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+        return false;
+    });
+
